b75745920f43e4287dff62fb1e21934031f1d975
Brendan <shaefferbrendan@gmail.com>
2024-01-14 00:00:37 -0500

    successfully uploaded data from apis to mongodb with refactored single function from 2 separate functions, resolved error when trying to send data from dummyjson api

--- backend/config/database.js
+++ backend/config/database.js
@@ -12,58 +12,35 @@ db.on('connected', function(){
     console.log(`Connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
 })
 
-const dataBase = client.db("product-compare");
-const coll = dataBase.collection("products");
+async function sendData(){
+    const url1 = 'https://fakestoreapi.com/products';
+    const url2 = 'https://dummyjson.com/products';
 
-
-// async function sendData(){
-//     const url1 = 'https://fakestoreapi.com/products';
-//     const url2 = 'https://fakestoreapi.com/products';
-//     try{
-       
-//         await client.connect();
-//         const dataBase = client.db("product-compare");
-//         const coll = dataBase.collection("products");
-
-//         const fakeStoreData = await fetch(url1);
-//         const fakeProducts = await fakeStoreData.json();
-
-//         const dummyData = await fetch(url2);
-//         const dumProducts = await dummyData.json();
-
-//         if(coll){
-//             const resultF = await coll.insertMany(fakeProducts);
-//             const resultD = await coll.insertMany(dumProducts);
-                
-//             console.log(resultF.insertedIds, resultD.insertedIds);
-//         }else{
-//             console.log('data alreayd exists, none will be sent from api')
-//         }
-
-//     }catch(err){
-//         console.log(err);
-//     }finally{
-//         await client.close();
-//     }
-
-// }
-// sendData();
-
-async function sendFData(){
-    const url = 'https://fakestoreapi.com/products'
     try{
+        const dataBase = client.db("product-compare");
+        const coll = dataBase.collection("products");
+        const options = {ordered: true}
+
         await client.connect();
-        const fakeStoreData = await fetch(url);
+        const fakeStoreData = await fetch(url1);
         const fakeProducts = await fakeStoreData.json();
+
+        const dummyData = await fetch(url2);
+        const dummyProducts = await dummyData.json();
         // console.log("FakeStore API Data:", fakeProducts);
-        db.collection.count(function (err, count){
-            if(!err && count === 0){
-                const resultF = coll.insertMany(fakeProducts);
-                console.log(resultF.insertedIds);
-            }else{
-                console.log('data alreayd exists, none will be sent from api')
-            }
-        });
+
+        const result = await coll.find({}).toArray();
+        result.length;
+        
+        if(result.length === 0){
+            const resultF = await coll.insertMany(fakeProducts, options);
+            console.log(resultF.insertedIds, "FakeStore data uploaded to database");
+
+            const resultD = await coll.insertMany(dummyProducts.products, options);
+            console.log(resultD.insertedIds, "Dummy data uploaded to database");
+        }else{
+            console.log('data alreayd exists, none will be sent from api');
+        }
 
     }catch(err){
         console.log(err)
@@ -71,29 +48,6 @@ async function sendFData(){
         await client.close();
     }
 }
-
-async function sendDData(){
-    const url = 'https://dummyjson.com/products'
-    try{
-        await client.connect();
-        const dummyData = await fetch(url);
-        const dummyProducts = await dummyData.json();
-        // console.log("DummyJSON API Data:", dummyProducts);
-        db.collection.count( function (err, count){
-            if(!err && count === 0){
-                const resultD = coll.insertMany(dummyProducts);
-                console.log(resultD.insertedIds);
-            }else{
-                console.log('data alreayd exists, none will be sent from api');
-            }
-        });
-        
-    }catch(err){
-        console.logl(err)
-    }finally{
-        await client.close();
-    }
-}
   
-sendFData();
-sendDData();
+sendData();
+
